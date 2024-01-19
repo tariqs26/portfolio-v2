674bc39af2a7cd3d2e4b06105f9fb24fcb05cbe0
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-19 00:40:55 -0500

    feat: add social links

---
+++ src/components/Socials.tsx
@@ -0,0 +1,21 @@
+import { socials } from "@/content/socials"
+import Link from "next/link"
+
+export default function Socials() {
+  return (
+    <section className="flex items-center gap-2">
+      {socials.map((social) => (
+        <Link
+          key={social.href}
+          href={social.href}
+          aria-label={social.label}
+          target="_blank"
+          rel="noreferrer"
+          className="text-muted-foreground transition hover:-translate-y-1 hover:text-foreground pb-1"
+        >
+          {social.icon}
+        </Link>
+      ))}
+    </section>
+  )
+}
---
+++ src/content/socials.tsx
@@ -0,0 +1,14 @@
+import { Github, LinkedIn } from "@/components/Icons"
+
+export const socials = [
+  {
+    href: "https://github.com/tariqs26",
+    label: "Github",
+    icon: <Github />,
+  },
+  {
+    href: "https://www.linkedin.com/in/saad-tariq-cs/",
+    label: "LinkedIn",
+    icon: <LinkedIn />,
+  },
+]
